f600849c56ebd5d2f198ef8142cee2448c4a314f
darma <damabigma@gmail.com>
2021-08-30 18:48:12 +0800

    feat(use-confirm): 提示窗口hook

--- src/render/composition/index.ts
+++ src/render/composition/index.ts
@@ -4,5 +4,6 @@ export { default as useTimeout } from './hooks/useTimeout'
 export { default as useWindowSize } from './hooks/useWindowSize'
 export { default as useMedia } from './hooks/useMedia'
 export { default as useMountedState } from './hooks/useMountedState'
+export { default as useConfirm } from './hooks/useConfirm'
 
 export { default as useScheme } from './b-hooks/useScheme'
